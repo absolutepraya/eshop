408c991e83f34ef6bc908f184ee95876d99bf621
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2025-02-11 19:14:15 +0700

    add delete method to ProductService and implement in ProductServiceImpl

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -42,4 +42,9 @@ public class ProductServiceImpl implements ProductService {
         }
         return null;
     }
+
+    @Override
+    public void delete(String id) {
+        productRepository.delete(id);
+    }
 }
