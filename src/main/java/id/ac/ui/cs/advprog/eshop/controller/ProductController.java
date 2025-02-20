7d9cd94af88d672e0c5c66486bf494961fc1482a
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2025-02-20 22:15:55 +0700

    refactor: reorder import statements in ProductController for consistency

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -2,11 +2,10 @@ package id.ac.ui.cs.advprog.eshop.controller;
 
 import java.util.List;
 
-import jakarta.validation.Valid;
-import org.springframework.validation.BindingResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import id.ac.ui.cs.advprog.eshop.service.ProductService;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/product")
