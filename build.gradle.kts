a2d6e910011c44711225dd777621d06a31b2b0db
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2025-02-21 07:24:17 +0700

    feat: add SonarQube workflow for code analysis and integrate SonarQube plugin in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,15 @@ plugins {
     jacoco
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.4"
+    id("org.sonarqube") version "6.0.1.5171"
+}
+
+sonar {
+	properties {
+		property("sonar.projectKey", "absolutepraya_eshop")
+		property("sonar.organization", "absolutepraya")
+		property("sonar.host.url", "https://sonarcloud.io")
+	}
 }
 
 group = "id.ac.ui.cs.advprog"
